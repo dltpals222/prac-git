a3bbc5fbf1ff2ca617f74fc9745495b5e5aa8807
blahblah <leesemin222@gmail.com>
2024-02-20 08:36:03 +0900

    create semantic tag

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //HTML의 root 아이디
 const root = document.getElementById('root');
+
+//make semantic tag as children of root element
+const header = document.createElement('header');
+const nav = document.createElement('nav');
+const main = document.createElement('main');
+const footer = document.createElement('footer');
